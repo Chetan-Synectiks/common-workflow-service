b0e13c4340c16a40e0f11d2a5690a04eb28c3a16
Ashutosh-Synectiks <ashutosh.shinde@synectiks.com>
2023-11-29 09:37:13 +0530

    updated get resources list according to Api Specs

--- workflow/resource/getResourceList.js
+++ workflow/resource/getResourceList.js
@@ -12,34 +12,46 @@ exports.getResourceList = async (event, context, callback) => {
 
     client.connect();
 
-
     let objReturn = {
         code: 200,
-        message: "resource search successfully",
+        message: "project search successfully",
         type: "object",
         object: []
     };
 
     try {
+        const resourceQueryResult = await client.query(`select * from resource_table`);
 
-        abc = await client.query(`select * from resource_table`);
+        // console.log(resourceQueryResult.rows);
 
-        console.log(abc.rows);
-        const ReturnedData = abc.rows.map(record => {
+        for (const record of resourceQueryResult.rows) {
 
-            let returnObj = {
-                Resource_id: record.resource_id,
-                Name: record.resource.name,
-                Image: record.resource.image,
-                Role: record.resource.role,
-                Project_list: record.resource.projects,
-                Email: record.resource.email
+            const projects = record.resource.projects;
+
+            const DataArray = [];
+
+            for (const projectId of projects) {
+                const Result = await client.query(`select project_id, project_table.details->>'name' as project_name from project_table where project_id = $1`, [projectId]);
+
+                //console.log('!!!!', Result.rows[0].project_name);
+
+                DataArray.push({
+                    projectId: projectId,
+                    project_name: Result.rows[0].project_name
+                });
             }
+            //console.log(DataArray);
+            let returnObj = {
+                resource_id: record.resource_id,
+                resource_name: record.resource.name,
+                resource_img_url: record.resource.image,
+                Role: record.resource.role,
+                resource_email: record.resource.email,
+                projects: DataArray
+            };
 
-            return returnObj;
-        })
-
-        objReturn.object = ReturnedData;
+            objReturn.object.push(returnObj);
+        }
 
         client.end();
 
@@ -52,10 +64,10 @@ exports.getResourceList = async (event, context, callback) => {
         };
 
     } catch (e) {
-
         objReturn.code = 400;
         objReturn.message = e;
         client.end();
+
         return {
             "statusCode": 400,
             "headers": {
