b7ef8fc49de0b70589a56db941e56deb73b0c588
Ashutosh-Synectiks <ashutosh.shinde@synectiks.com>
2023-12-06 15:23:28 +0530

    feat: Update usecase data with stages from projects_table

--- workflow/usecase/addusecase.js
+++ workflow/usecase/addusecase.js
@@ -1,13 +1,10 @@
 const { Client } = require('pg');
 
 exports.addusecase = async (event) => {
-    // Parse the incoming request body
     const requestBody = JSON.parse(event.body);
 
-    // Extract data from the request body
     const { project_id, created_by_id, usecase_name, assigned_to_id, description } = requestBody;
 
-    // PostgreSQL configuration
     const client = new Client({
         host: "localhost",
         port: "5432",
@@ -17,24 +14,80 @@ exports.addusecase = async (event) => {
     });
 
     try {
-
         await client.connect();
 
-        // Insert data into the usecases_table
+        await client.query('BEGIN');
+
         const result = await client.query(
             'INSERT INTO usecases_table (project_id, usecase) VALUES ($1, $2) RETURNING *',
             [project_id, {
                 name: usecase_name,
                 usecase_assignee_id: assigned_to_id,
                 description: description,
-                created_by_id: created_by_id, // Added created_by_id to the usecase JSONB
+                created_by_id: created_by_id,
+                start_date: "date",
+                end_date: "date",
+                creation_date: "date",
+                status: "",
+                current_stage: "stage_name",
             }]
         );
 
-        // Extract the inserted data from the result
         const insertedData = result.rows[0];
 
-        // Prepare the response
+        const projectStagesResult = await client.query(
+            'SELECT project->\'stages\' as stages FROM projects_table WHERE id = $1',
+            [project_id]
+        );
+
+        const stages = projectStagesResult.rows[0].stages;
+
+        const stagesData = {};
+        for (const stageName in stages) {
+            stagesData[stageName] = {
+                assigne_id: assigned_to_id,
+                checklists: stages[stageName].checklists.map((item, index) => ({
+                    item_id: index + 1,
+                    description: item,
+                    checked: false,
+                })),
+            };
+        }
+
+        await client.query(
+            'UPDATE usecases_table SET usecase = jsonb_set(usecase, $1, $2) WHERE id = $3',
+            ['{stages}', stagesData, insertedData.id]
+        );
+
+        for (const stageName in stages) {
+            const stage = stages[stageName];
+
+            for (const taskName of stage.tasks) {
+                const taskData = {
+                    usecase_id: insertedData.id,
+                    project_id: project_id,
+                    assignee_id: assigned_to_id,
+                    stage: stageName,
+                    task: {
+                        created_date: 'current-date',
+                        start_date: 'start-date',
+                        end_date: 'end-date',
+                        resource_start_date: 'resource-start-date',
+                        resource_end_date: 'resource-end-date',
+                        status: '',
+                        comments: [],
+                    },
+                };
+
+                await client.query(
+                    'INSERT INTO tasks_table (usecase_id, project_id, assignee_id, stage, task) VALUES ($1, $2, $3, $4, $5)',
+                    [taskData.usecase_id, taskData.project_id, taskData.assignee_id, taskData.stage, taskData.task]
+                );
+            }
+        }
+
+        await client.query('COMMIT');
+
         const response = {
             statusCode: 200,
             body: JSON.stringify({
@@ -49,15 +102,14 @@ exports.addusecase = async (event) => {
 
         return response;
     } catch (error) {
-        // Handle errors
         console.error('Error inserting data:', error);
+        await client.query('ROLLBACK');
 
         return {
             statusCode: 500,
             body: JSON.stringify({ message: 'Internal Server Error' }),
         };
     } finally {
-        // Close the database connection
         await client.end();
     }
 };
