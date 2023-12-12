1c176a5d014634869a3388ea2cd87315ef86529e
Ashutosh-Synectiks <ashutosh.shinde@synectiks.com>
2023-12-12 13:45:44 +0530

    feat:Updated addWorkflowToProject API

--- workflow/project/addWorkflowToProject.js
+++ workflow/project/addWorkflowToProject.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
+exports.addWorkflowToProject = async (event) => {
 
-exports.handler = async (event) => {
     const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
     const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
     const dbConfig = JSON.parse(configuration.SecretString);
-    
+
     const client = new Client({
         host: dbConfig.host,
         port: dbConfig.port,
@@ -13,40 +13,53 @@ exports.handler = async (event) => {
         user: dbConfig.engine,
         password: dbConfig.password
     });
+
+    const updateData = JSON.parse(event.body);
     try {
         await client
-        .connect()
-        .then(() => {
-            console.log("Connected to the database");
-        })
-        .catch((err) => {
-            console.log("Error connecting to the database. Error :" + err);
+            .connect()
+            .then(() => {
+                console.log("Connected to the database");
+            })
+            .catch((err) => {
+                console.log("Error connecting to the database. Error :" + err);
+            });
+
+        const workflowName = updateData.workflow_name;
+        const stages = updateData.stages;
+
+        const stageUpdates = Object.keys(stages).map((stageName) => {
+            const tasks = stages[stageName].tasks;
+            const checklists = stages[stageName].checklists;
+
+            return `"${stageName}": {
+                "tasks": ${JSON.stringify(tasks)},
+                "checklists": ${JSON.stringify(checklists)}
+            }`;
         });
-        
-        const projectId = event.queryStringParameters.project_id;
- 
-        // Fetch the existing JSON data from the database
-        const result = await client.query('SELECT id, project FROM projects_table WHERE id = $1', [projectId]);
-        const existingData = result.rows[0];
- 
-        // Parse the "workflows" object from the request body
-        const workflowsObject = JSON.parse(event.body);
- 
-        // Update the JSON data with the provided "workflows" object
-        existingData.project.workflows = workflowsObject;
- 
-        // Update the JSON data back to the database
-        await client.query('UPDATE projects_table SET project = $1 WHERE id = $2', [existingData.project, projectId]);
- 
+
+        await client.query(`
+            UPDATE projects_table
+            SET project = jsonb_set(
+                project,
+                '{workflows, ${workflowName}}',
+                '{"stages": {${stageUpdates.join(',')}}}',
+                true
+            )
+            WHERE id = $1;
+        `, [updateData.project_id]);
+
+        console.log(`Workflows updated for project: ${updateData.project_id}, workflow: ${workflowName}`);
+
         return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Data updated successfully' }),
+            statusCode: 201,
+            body: JSON.stringify('Workflows updated successfully'),
         };
     } catch (error) {
-        console.error('Error updating data:', error);
+        console.error('Error updating workflows data:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Internal Server Error' }),
+            body: JSON.stringify('Internal Server Error'),
         };
     } finally {
         await client.end();
