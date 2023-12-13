4e2826782a10f3004f2dbf5cb17d1974e504e5ca
sukantahalder123 <haldersukanta13@gmail.com>
2023-12-13 12:33:02 +0530

    feat: updated getProjects api

--- workflow/project/getProjects.js
+++ workflow/project/getProjects.js
@@ -1,11 +1,10 @@
 const { Client } = require('pg');
 const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
-
 exports.handler = async (event) => {
     
-  const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
-  const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
-  const dbConfig = JSON.parse(configuration.SecretString);
+    const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
+    const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
+    const dbConfig = JSON.parse(configuration.SecretString);
     
     const client = new Client({
         host: dbConfig.host,
@@ -13,7 +12,7 @@ exports.handler = async (event) => {
         database: 'workflow',
         user: dbConfig.engine,
         password: dbConfig.password
-      });
+    });
 
     try {
         await client
@@ -25,64 +24,67 @@ exports.handler = async (event) => {
             console.log("Error connecting to the database. Error :" + err);
         });
 
-        const projectFilter = event.queryStringParameters && event.queryStringParameters.status;
-        console.log(projectFilter);
+        const projectStatusFilter = event.queryStringParameters && event.queryStringParameters.status;
 
-        let projectsQuery = `
-            SELECT
-                projects_table.id AS project_id,
-                projects_table.project->>'name' AS project_name,
-                projects_table.project->>'project_icon' AS project_icon_url,
-                COUNT(usecases_table.project_id) AS total_usecases,
-                projects_table.project->>'status' AS project_status,
-                SUM(CASE WHEN jsonb_typeof(roles) = 'array' THEN jsonb_array_length(roles) ELSE 0 END) AS total_resources
-            FROM
-                projects_table
-            LEFT JOIN
-                usecases_table ON projects_table.id = usecases_table.project_id
-            LEFT JOIN LATERAL (
-                SELECT value->'role' AS roles
-                FROM jsonb_each(projects_table.project->'teams')
-            ) AS team_roles ON true
-        `;
-
-        if (projectFilter) {
-            projectsQuery += ` WHERE projects_table.project->>'status' = '${projectFilter}'`;
-        }
-
-        projectsQuery += `
-            GROUP BY
-                projects_table.id, projects_table.project
-        `;
+        const projectsQuery = projectStatusFilter
+            ? `SELECT * FROM projects_table WHERE project->>'status' = '${projectStatusFilter}'`
+            : 'SELECT * FROM projects_table';
+        console.log(projectsQuery)
+        const usecasesQuery = 'SELECT * FROM usecases_table';
 
         const projectsResult = await client.query(projectsQuery);
+        const usecasesResult = await client.query(usecasesQuery);
 
-        const outputData = projectsResult.rows.map((project) => ({
-            project_id: project.project_id,
-            project_name: project.project_name,
-            project_icon_url: project.project_icon_url,
-            total_usecases: project.total_usecases || 0,
-            total_resources: project.total_resources || 0,
-            project_status: project.project_status
-        }));
+        const outputData = processDatabaseData(projectsResult.rows, usecasesResult.rows);
 
         return {
             statusCode: 200,
             headers: {
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
             },
-            body: JSON.stringify(outputData)
+            body: JSON.stringify(outputData),
         };
     } catch (error) {
         console.error('Error executing query:', error);
         return {
             statusCode: 500,
             headers: {
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
             },
-            body: JSON.stringify({ message: 'Internal Server Error' })
+            body: JSON.stringify({ message: 'Internal Server Error' }),
         };
     } finally {
         await client.end();
     }
 };
+
+function processDatabaseData(projects, usecases) {
+    const outputData = projects.map((project) => {
+        const projectUsecases = usecases.filter(u => u.project_id === project.id);
+        const totalUsecases = projectUsecases.length;
+
+        const projectData = project.project || {}; 
+
+        const totalRoles = projectData.teams
+            ? Object.values(projectData.teams).reduce((acc, team) => {
+                if (team.roles) {
+                    Object.values(team.roles).forEach(roleNames => {
+                        acc += roleNames.length;
+                    });
+                }
+                return acc;
+            }, 0)
+            : 0;
+
+        return {
+            project_id: project.id,
+            project_name: projectData.name || '', 
+            project_status: projectData.status || '', 
+            total_resources: totalRoles,
+            total_usecases: totalUsecases
+        };
+    });
+
+    return outputData;
+}
+
