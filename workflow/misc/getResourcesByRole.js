8570df2808d38682a5dcccf8e0bb4e1b1829629f
sohailali20 <153061595+SohailAli20@users.noreply.github.com>
2023-12-15 11:33:16 +0530

    implemented getResourcesByRole API

--- workflow/misc/getResourcesByRole.js
+++ workflow/misc/getResourcesByRole.js
@@ -15,19 +15,8 @@ exports.handler = async (event) => {
     });
 
     const project_id = event.queryStringParameters.project_id;
-    const team_name = event.queryStringParameters.team_name;
     const role = event.queryStringParameters.role;
     const resourceName = event.queryStringParameters.resource_name;
-
-    if (!project_id || !team_name || !role) {
-        return {
-            statusCode: 400,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({ message: 'Missing required parameters: project_id, team_name, role' }),
-        };
-    }
 
     try {
         await client
@@ -39,61 +28,43 @@ exports.handler = async (event) => {
                 console.log("Error connecting to the database. Error :" + err);
             });
 
-        const teamDataResult = await client.query(
-            'SELECT project->\'teams\' as teams FROM projects_table WHERE id = $1',
-            [project_id]
-        );
-
-        const teamsData = teamDataResult.rows[0]?.teams || {};
-
-        if (teamsData[team_name]) {
-            const teamRoles = teamsData[team_name].roles || {};
-            if (teamRoles[role]) {
-                const resourceIds = teamRoles[role];
-
-                let query = 'SELECT id, resource->>\'name\' as name, resource->>\'image_url\' as image_url FROM resources_table WHERE id = ANY($1)';
-
-                const queryParams = [resourceIds];
-
-                if (resourceName) {
-                    query += ' AND resource->>\'name\' ILIKE $2';
-                    queryParams.push(`%${resourceName}%`);
-                } else {
-                    query += ' LIMIT 10';
-                }
-
-                const resourceResult = await client.query(query, queryParams);
-                const resultdata = resourceResult.rows;
-                const response = {
-                    statusCode: 200,
-                    headers: {
-                        'Access-Control-Allow-Origin': '*',
-                    },
-                    body: JSON.stringify(resultdata),
-                };
-
-                return response;
-            } else {
-                return {
-                    statusCode: 404,
-                    headers: {
-                        'Access-Control-Allow-Origin': '*',
-                    },
-                    body: JSON.stringify({ message: 'Role not found in the specified team' }),
-                };
-            }
-        } else {
-            return {
-                statusCode: 404,
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                },
-                body: JSON.stringify({ message: 'Team not found' }),
+        const result = await client.query(
+            `SELECT (project->'teams'->jsonb_object_keys(project->'teams')->'roles'->$1) AS roleResources
+            FROM projects_table
+            WHERE id = $2`,
+            [role,project_id]
+            );
+        const roleResources = result.rows[0].roleresources
+        const resourceQuery = `select resource->>'name' as name,
+                                resource->>'image' as image_url,
+                                resource->>'email' as email
+                                from resources_table 
+                                where id = $1`;
+        let resourceArray = [];
+        for(const resource of roleResources){
+            const result =await client.query(resourceQuery,[resource]);
+            const name = result.rows[0].name
+            const image = result.rows[0].image_url
+            const email = result.rows[0].email
+            const resourceObj = {
+                id: resource,
+                name: name,
+                image_url: image,
+                email: email
             };
+            resourceArray.push(resourceObj)
         }
+        return {
+            statuscode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(resourceArray)
+
+        };
+
     } catch (error) {
         console.error('Error fetching resources:', error);
-
         return {
             statusCode: 500,
             headers: {
