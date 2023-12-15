7d0300340fca9da2513e7d44bea0be651e876f24
sohailali20 <153061595+SohailAli20@users.noreply.github.com>
2023-12-15 14:45:22 +0530

    Implemented: getTaskDetails and startTask APIs

--- workflow/task/getTaskDetails.js
+++ workflow/task/getTaskDetails.js
@@ -1,91 +1,93 @@
-const { Client } = require('pg');
-const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
+const { Client } = require("pg");
+const {
+	SecretsManagerClient,
+	GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
 
 exports.handler = async (event) => {
+	const secretsManagerClient = new SecretsManagerClient({
+		region: "us-east-1",
+	});
+	const configuration = await secretsManagerClient.send(
+		new GetSecretValueCommand({ SecretId: "serverless/lambda/credintials" })
+	);
+	const dbConfig = JSON.parse(configuration.SecretString);
 
-    const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
-    const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
-    const dbConfig = JSON.parse(configuration.SecretString);
+	const client = new Client({
+		host: dbConfig.host,
+		port: dbConfig.port,
+		database: "workflow",
+		user: dbConfig.engine,
+		password: dbConfig.password,
+	});
 
-    const client = new Client({
-        host: dbConfig.host,
-        port: dbConfig.port,
-        database: 'workflow',
-        user: dbConfig.engine,
-        password: dbConfig.password
-    });
+	try {
+		await client
+			.connect()
+			.then(() => {
+				console.log("Connected to the database");
+			})
+			.catch((err) => {
+				console.log("Error connecting to the database. Error :" + err);
+			});
 
-try {
+		const resource_id = event.queryStringParameters.resource_id;
+		const query = `
+                    SELECT * 
+                    FROM tasks_table 
+                    WHERE assignee_id = $1`;
 
-  await client
-  .connect()
-  .then(() => {
-      console.log("Connected to the database");
-  })
-  .catch((err) => {
-      console.log("Error connecting to the database. Error :" + err);
-  });
+		const result = await client.query(query, [resource_id]);
 
-  const resource_id = event.queryStringParameters.resource_id;
-  const query = `
-      SELECT * 
-      FROM tasks_table 
-      WHERE assignee_id = $1
-  `;
+		if (result.rowCount === 0) {
+			return {
+				statusCode: 204,
+				body: JSON.stringify({
+					message: "No tasks present",
+				}),
+			};
+		}
 
-  const result = await client.query(query, [resource_id]);
+		const tasksList = result.rows.map(async (row) => {
+			const projectResult = await client.query(
+				`SELECT
+                (project->'name') AS name
+                FROM projects_table WHERE id = $1`,
+				[row.project_id]
+			);
+			const projectName = projectResult.rows[0].name;
 
-  if (result.rows.length === 0) {
-      return {
-          statusCode: 404,
-          body: JSON.stringify({ message: 'No tasks found for the given resource ID' }),
-      };
-  }
+			const assignedBy = await client.query(
+				`SELECT
+                (resource->'name') AS name
+                FROM resources_table WHERE id = $1`,
+				[row.task.assigned_by_id]
+			);
 
-  const tasksList = result.rows.map(async row => {
-      const projectResult = await client.query('SELECT * FROM projects_table WHERE id = $1', [row.project_id]);
-      const project = projectResult.rows[0].project;
+			return {
+				task_id: row.id,
+				task_name: row.task.name,
+				project_id: row.project_id,
+				project_name: projectName,
+				assigned_by_id: row.task.assigned_by_id,
+				assigned_by_name: assignedBy.rows[0].name,
+				task_assigned_date: row.task.task_assigned_date,
+			};
+		});
 
-      const assignedByResult = await client.query('SELECT * FROM resources_table WHERE id = $1', [row.task.assigned_by_id]);
+		const resolvedTasksList = await Promise.all(tasksList);
 
-      if (assignedByResult.rows.length === 0) {
-          return {
-              task_id: row.id,
-              task_name: row.task.name, 
-              project_id: row.project_id,
-              project_name: project.name, 
-              assigned_by_id: row.task.assigned_by_id,
-              assigned_by_name: 'Unknown', 
-              task_assigned_date: row.task.task_assigned_date,
-          };
-      }
-
-      const assignedByResource = assignedByResult.rows[0].resource;
-
-      return {
-          task_id: row.id,
-          task_name: row.task.name, 
-          project_id: row.project_id,
-          project_name: project.name, 
-          assigned_by_id: row.task.assigned_by_id,
-          assigned_by_name: assignedByResource.name, 
-          task_assigned_date: row.task.task_assigned_date,
-      };
-  });
-
-  const resolvedTasksList = await Promise.all(tasksList);
-
-  return {
-      statusCode: 200,
-      body: JSON.stringify(resolvedTasksList),
-  };
-} catch (error) {
-  console.error('Error executing query:', error);
-  return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Internal Server Error' }),
-  };
-} finally {
-  await client.end();
-}
+		return {
+			statusCode: 200,
+			body: JSON.stringify(resolvedTasksList),
+		};
+	} catch (error) {
+		console.error("Error executing query:", error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: "Internal Server Error" }),
+		};
+	} finally {
+		await client.end();
+	}
 };
--- workflow/task/startTask.js
+++ workflow/task/startTask.js
@@ -1,79 +1,115 @@
-const { Client } = require('pg');
-const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
+const { Client } = require("pg");
+const {
+	SecretsManagerClient,
+	GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
 exports.handler = async (event) => {
+	const secretsManagerClient = new SecretsManagerClient({
+		region: "us-east-1",
+	});
+	const configuration = await secretsManagerClient.send(
+		new GetSecretValueCommand({ SecretId: "serverless/lambda/credintials" })
+	);
+	const dbConfig = JSON.parse(configuration.SecretString);
 
-    const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
-    const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
-    const dbConfig = JSON.parse(configuration.SecretString);
+	const client = new Client({
+		host: dbConfig.host,
+		port: dbConfig.port,
+		database: "workflow",
+		user: dbConfig.engine,
+		password: dbConfig.password,
+	});
 
-    const client = new Client({
-        host: dbConfig.host,
-        port: dbConfig.port,
-        database: 'workflow',
-        user: dbConfig.engine,
-        password: dbConfig.password
-    });
+	const resource_id = event.queryStringParameters.resource_id;
+	const task_id = event.pathParameters.task_id;
+	try {
+		await client
+			.connect()
+			.then(() => {
+				console.log("Connected to the database");
+			})
+			.catch((err) => {
+				console.log("Error connecting to the database. Error :" + err);
+			});
 
-    const requestBody = JSON.parse(event.body);
-    const { task_id, start_date } = requestBody;
-    const { resource_id } = event.queryStringParameters;
-    try {
-        await client
-            .connect()
-            .then(() => {
-                console.log("Connected to the database");
-            })
-            .catch((err) => {
-                console.log("Error connecting to the database. Error :" + err);
-            });
+		const taskDetailsQuery = `
+                SELECT
+                task AS task
+                FROM tasks_table 
+                WHERE id = $1`;
 
-            const startingTaskQuery = `
-            UPDATE tasks_table AS t
-            SET task = jsonb_set(
-                jsonb_set(t.task, '{task_assigned_date}', $1::jsonb),
-                '{status}', '"InProgress"'
-            )
-            FROM resources_table AS r
-            WHERE 
-                t.id = $2
-                AND t.assignee_id = $3
-                AND r.id = $4
-        `;
-
-        await client.query(startingTaskQuery, [JSON.stringify(start_date), task_id, resource_id, resource_id]);
-
-        const updateResourceQuery = `
-            UPDATE resources_table 
-            SET resource = jsonb_set(
-                resource, '{current_task}', 
-                jsonb_build_object('task_id', $1::text, 'task_name', t.task->>'name')
-            )
-            FROM tasks_table AS t
-            WHERE 
-                t.id = $2
-                AND t.assignee_id = $3
-                AND resources_table.id = $4
-        `;
-
-        await client.query(updateResourceQuery, [task_id, task_id, resource_id, resource_id]);
-
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({ message: "Task started" })
-        };
-    } catch (error) {
-        console.error("error", error);
-        return {
-            statusCode: 500,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({ message: "Error while starting task " })
-        };
-    } finally {
-        await client.end();
-    }
+		const result = await client.query(taskDetailsQuery, [task_id]);
+		const task = result.rows[0].task;
+		task.status = "in progress";
+		task.resource_start_date = new Date();
+		await client.query("BEGIN");
+		const taskUpdateQuery = `
+            UPDATE tasks_table
+            SET task = $1
+            WHERE id = $2
+            `;
+		const updateResult = await client.query(taskUpdateQuery, [
+			task,
+			task_id,
+		]);
+		if (updateResult.rowCount === 0) {
+			await client.query("ROLLBACK");
+			return {
+				statusCode: 500,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+				},
+				body: JSON.stringify({ message: "Unable to start task" }),
+			};
+		}
+		const current_task = {
+			task_id: task_id,
+			task_name: task.name,
+		};
+		const updateCurrentTaskQuery = `
+                    UPDATE resources_table
+                    SET resource = jsonb_set(
+                        resource,
+                        '{current_task}',
+                        $1::jsonb,
+                        true
+                    )
+                    WHERE id = $2`;
+		const updateCurrentTaskResult = await client.query(
+			updateCurrentTaskQuery,
+			[current_task, resource_id]
+		);
+		if (updateCurrentTaskResult.rowCount === 0) {
+			await client.query("ROLLBACK");
+			return {
+				statusCode: 500,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+				},
+				body: JSON.stringify({
+					message: "Unable to update current task",
+				}),
+			};
+		}
+		await client.query("COMMIT");
+		return {
+			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+			body: JSON.stringify({ message: "Task started" }),
+		};
+	} catch (error) {
+		console.error("error", error);
+		await client.query("ROLLBACK");
+		return {
+			statusCode: 500,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+			body: JSON.stringify({ message: "Error while starting task " }),
+		};
+	} finally {
+		await client.end();
+	}
 };
