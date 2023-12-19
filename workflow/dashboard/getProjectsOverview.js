73355dc1df3ea1db38855b7448d00713a61d8487
sohailali20 <153061595+SohailAli20@users.noreply.github.com>
2023-12-19 16:44:59 +0530

    implementeed: getProjectsOverview API

--- workflow/dashboard/getProjectsOverview.js
+++ workflow/dashboard/getProjectsOverview.js
@@ -1,102 +1,98 @@
-const { Client } = require('pg');
-const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
+const { Client } = require("pg");
+const {
+	SecretsManagerClient,
+	GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
 exports.handler = async (event) => {
+	const secretsManagerClient = new SecretsManagerClient({
+		region: "us-east-1",
+	});
+	const configuration = await secretsManagerClient.send(
+		new GetSecretValueCommand({ SecretId: "serverless/lambda/credintials" })
+	);
+	const dbConfig = JSON.parse(configuration.SecretString);
 
-    const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
-    const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
-    const dbConfig = JSON.parse(configuration.SecretString);
+	const client = new Client({
+		host: dbConfig.host,
+		port: dbConfig.port,
+		database: "workflow",
+		user: dbConfig.engine,
+		password: dbConfig.password,
+	});
 
-    const client = new Client({
-        host: dbConfig.host,
-        port: dbConfig.port,
-        database: 'workflow',
-        user: dbConfig.engine,
-        password: dbConfig.password
-    });
+	try {
+		await client
+			.connect()
+			.then(() => {
+				console.log("Connected to the database");
+			})
+			.catch((err) => {
+				console.log("Error connecting to the database. Error :" + err);
+			});
 
-    try {
-        await client
-            .connect()
-            .then(() => {
-                console.log("Connected to the database");
-            })
-            .catch((err) => {
-                console.log("Error connecting to the database. Error :" + err);
-            });
+		const status = event.queryStringParameters?.project_status ?? null;
 
-        const projectStatusFilter = event.queryStringParameters && event.queryStringParameters.project_status;
-
-        const projectsQuery = projectStatusFilter
-            ? `SELECT * FROM projects_table WHERE project->>'status' = '${projectStatusFilter}'`
-            : 'SELECT * FROM projects_table';
-
-        const usecasesQuery = 'SELECT * FROM usecases_table';
-        const tasksQuery = 'SELECT * FROM tasks_table';
-
-        const projectsResult = await client.query(projectsQuery);
-        const usecasesResult = await client.query(usecasesQuery);
-        const tasksResult = await client.query(tasksQuery);
-
-
-        const outputData = processDatabaseData(projectsResult.rows, usecasesResult.rows, tasksResult.rows);
-
-
-        //console.log(outputData);
-
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify(outputData),
-        };
-    } catch (error) {
-        console.error('Error executing query:', error);
-        return {
-            statusCode: 500,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({ message: 'Internal Server Error' }),
-        };
-    } finally {
-        await client.end();
-    }
+		let query = `
+                    select 
+                        p.id as project_id,
+                        (p.project->>'name') as project_name,
+                        (p.project->>'status') as status,
+                        (p.project->>'end_date') as due_date,
+                        COUNT(distinct u.id) as total_usecases,
+                        COUNT(t.id) as total_tasks,
+                        COUNT(t.id) FILTER (WHERE t.task->>'status' = 'completed') as tasks_completed
+                        from projects_table as p 
+                    left join
+                        usecases_table as u on p.id = u.project_id 
+                    left join
+                        tasks_table as t on u.id = t.usecase_id and p.id = t.project_id
+                    `;
+        const queryParams = []
+		if (status !== null) {
+			query += `
+                    where
+                        (p.project->>'status' = $1)`;
+                        queryParams.push(status)
+		}
+		query += `
+                    group by 
+                        p.id`;
+		const result = await client.query(query, queryParams);
+		const projectsOverview = result.rows.map(
+			({
+				project_id,
+				project_name,
+				status,
+				due_date,
+				total_usecases,
+				total_tasks,
+				tasks_completed,
+			}) => ({
+				project_id,
+				project_name,
+				status,
+				due_date,
+				total_usecases,
+				completed_tasks_per: (total_tasks !== 0) ? (tasks_completed / total_tasks) * 100 : 0
+			})
+		);
+		return {
+			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+			body: JSON.stringify(projectsOverview),
+		};
+	} catch (error) {
+		console.error("Error executing query:", error);
+		return {
+			statusCode: 500,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+			body: JSON.stringify({ message: "Internal Server Error" }),
+		};
+	} finally {
+		await client.end();
+	}
 };
-
-
-function processDatabaseData(projects, usecases, tasks) {
-
-    const outputData = projects.map((project) => {
-        const projectUsecases = usecases.filter(u => u.project_id === project.id);
-        const projectTasks = tasks.filter(t => t.project_id === project.id);
-
-        const totalUsecases = projectUsecases.length;
-        const completedUsecases = projectUsecases.filter(u => u.usecase.status === 'completed').length;
-        const completedTasksPercentage = calculateCompletedTasksPercentage(projectTasks);
-
-        return {
-            project_id: project.id,
-            project_name: project.project.name,
-            status: project.project.status,
-            total_usecases: totalUsecases,
-            completed_usecases: completedUsecases,
-            due_date: project.project.end_date,
-            completed_tasks_percentage: completedTasksPercentage,
-        };
-    });
-
-    return outputData;
-}
-
-function calculateCompletedTasksPercentage(tasks) {
-
-    const totalTasks = tasks.length;
-    const completedTasks = tasks.filter(t => t.task.status === 'completed').length;
-
-    if (totalTasks === 0) {
-        return 0;
-    }
-
-    return Math.round((completedTasks / totalTasks) * 100);
-}
