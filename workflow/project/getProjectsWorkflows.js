9dcec80eb22f9f238e807a107e87e2d3f26c40a3
Ashutosh-Synectiks <ashutosh.shinde@synectiks.com>
2023-12-29 17:41:06 +0530

    refactor: Abstracted database connection

--- workflow/project/getProjectsWorkflows.js
+++ workflow/project/getProjectsWorkflows.js
@@ -1,8 +1,4 @@
-const { Client } = require('pg');
-const {
-    SecretsManagerClient,
-    GetSecretValueCommand,
-} = require("@aws-sdk/client-secrets-manager");
+const { connectToDatabase } = require("../db/dbConnector");
 exports.handler = async (event) => {
     const project_id = event.queryStringParameters?.project_id;
     if (!project_id) {
@@ -15,40 +11,15 @@ exports.handler = async (event) => {
         };
     }
 
-    const secretsManagerClient = new SecretsManagerClient({
-        region: "us-east-1",
-    });
-    const configuration = await secretsManagerClient.send(
-        new GetSecretValueCommand({ SecretId: "serverless/lambda/credintials" })
-    );
-    const dbConfig = JSON.parse(configuration.SecretString);
-
-    const client = new Client({
-        host: dbConfig.host,
-        port: dbConfig.port,
-        database: "workflow",
-        user: dbConfig.engine,
-        password: dbConfig.password,
-    });
-
     try {
 
-        await client
-            .connect()
-            .then(() => {
-                console.log("Connected to the database");
-            })
-            .catch((err) => {
-                console.log("Error connecting to the database. Error :" + err);
-            });
+        const client = await connectToDatabase();
 
         const projectQuery = `
         SELECT
             p.id AS project_id,
             p.project->>'name' AS project_name,
-            p.project->'last_updated'->>'upadted_by_id' AS updated_by_id,
-            p.project->'last_updated'->>'upadted_by_name' AS updated_by_name,
-            p.project->'last_updated'->>'updated_time' AS updated_time,
+            p.project->'last_updated' AS last_updated,
             p.project->>'project_description' AS project_description
         FROM projects_table p
         WHERE p.id = $1
@@ -83,9 +54,7 @@ exports.handler = async (event) => {
         const response = {
             project_id: project.project_id,
             project_name: project.project_name,
-            updated_by_id: project.updated_by_id,
-            updated_by_name: project.updated_by_name,
-            updated_time: project.updated_time,
+            last_updated: project.last_updated,
             project_description: project.project_description,
             workflows: workflows,
         };
--- workflow/project/projectsResourcesTasksStatus.js
+++ workflow/project/projectsResourcesTasksStatus.js
@@ -1,8 +1,4 @@
-const { Client } = require("pg");
-const {
-    SecretsManagerClient,
-    GetSecretValueCommand,
-} = require("@aws-sdk/client-secrets-manager");
+const { connectToDatabase } = require("../db/dbConnector");
 exports.handler = async (event) => {
     const projectId = event.queryStringParameters?.project_id;
     if (!projectId) {
@@ -18,31 +14,8 @@ exports.handler = async (event) => {
     const fromDate = event.queryStringParameters?.from_date ?? null;
     const toDate = event.queryStringParameters?.to_date ?? null;
 
-    const secretsManagerClient = new SecretsManagerClient({
-        region: "us-east-1",
-    });
-    const configuration = await secretsManagerClient.send(
-        new GetSecretValueCommand({ SecretId: "serverless/lambda/credintials" })
-    );
-    const dbConfig = JSON.parse(configuration.SecretString);
-
-    const client = new Client({
-        host: dbConfig.host,
-        port: dbConfig.port,
-        database: "workflow",
-        user: dbConfig.engine,
-        password: dbConfig.password,
-    });
-
     try {
-        await client
-            .connect()
-            .then(() => {
-                console.log("Connected to the database");
-            })
-            .catch((err) => {
-                console.log("Error connecting to the database. Error :" + err);
-            });
+        const client = await connectToDatabase();
 
         let query = `SELECT
                         r.id AS resource_id,
