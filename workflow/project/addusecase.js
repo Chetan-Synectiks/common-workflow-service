60f4a34bdf9ea7b3e177d3f5867ef029f868af39
SohailAli20 <mohammad.ali@synectiks.com>
2024-01-16 13:10:50 +0530

    changes: used step functions to create new workflow. added: compelte task api to complete individual task in the step machine

--- workflow/project/addusecase.js
+++ workflow/project/addusecase.js
@@ -1,38 +1,10 @@
-const { Client } = require('pg');
-const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
+const { connectToDatabase } = require("../db/dbConnector");
+const { SFNClient, CreateStateMachineCommand } = require("@aws-sdk/client-sfn");
 exports.handler = async (event) => {
-
-    const secretsManagerClient = new SecretsManagerClient({ region: 'us-east-1' });
-    const configuration = await secretsManagerClient.send(new GetSecretValueCommand({ SecretId: 'serverless/lambda/credintials' }));
-    const dbConfig = JSON.parse(configuration.SecretString);
-
-    const client = new Client({
-        host: dbConfig.host,
-        port: dbConfig.port,
-        database: 'workflow',
-        user: dbConfig.engine,
-        password: dbConfig.password
-    });
+    const client = await connectToDatabase();
 
     const requestBody = JSON.parse(event.body);
     const { project_id, created_by_id, usecase_name, assigned_to_id, description, workflow_name } = requestBody;
-    await client
-		.connect()
-		.then(() => {
-			console.log("Connected to the database");
-		})
-		.catch((err) => {
-			console.log("Error connecting to the database. Error :" + err);
-			return {
-				statusCode: 200,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-				},
-				body: JSON.stringify({
-					message: "Internal server error : " + err.message,
-				}),
-			};
-		});
     try {
         const projectQuery = `
             SELECT project->'workflows'->$1 AS workflow
