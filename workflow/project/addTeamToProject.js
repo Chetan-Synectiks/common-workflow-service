147ef8a9d3dade66342ccfe959ccec2089a2f2cb
Chetan Meegada <chetan.meegada@synectiks.com>
2024-04-01 16:01:02 +0530

    Update: added authontication and middy to the projects folder apis

--- workflow/project/addTeamToProject.js
+++ workflow/project/addTeamToProject.js
@@ -1,60 +1,35 @@
-const { connectToDatabase } = require("../db/dbConnector");
-const { z } = require("zod");
-exports.handler = async (event) => {
-	const project_id = event.pathParameters?.id ?? null;
-	const projectIdSchema = z.string().uuid({message : "Invalid project id"})
-	const isUuid = projectIdSchema.safeParse(project_id)
-	if(!isUuid.success){
-		return {
-			statusCode: 400,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Credentials": true,
-			},
-			body: JSON.stringify({
-				error: isUuid.error.issues[0].message
-			}),
-		};
+const { connectToDatabase } = require("../db/dbConnector")
+const { z } = require("zod")
+const middy = require("@middy/core")
+const { authorize } = require("../util/authorizer")
+const { errorHandler } = require("../util/errorHandler")
+const { bodyValidator } = require("../util/bodyValidator")
+const reqSchema = z.object({
+	team_name: z.string().min(3, {
+		message: "Team name must be atleast 3 charachters long",
+	}),
+	created_by_id: z.string().uuid({
+		message: "Invalid created by id",
+	}),
+	roles: z.array(z.record(z.string(), z.string().uuid().array().nonempty())),
+})
+const { pathParamsValidator } = require("../util/pathParamsValidator")
+const idSchema = z.object({
+	id: z.string().uuid({ message: "Invalid project id" }),
+})
+exports.handler = middy(async (event, context) => {
+	context.callbackWaitsForEmptyEventLoop = false
+	const project_id = event.pathParameters?.id ?? null
+	const requestBody = JSON.parse(event.body)
+	const { team_name, created_by_id, roles } = requestBody
+	const team = {
+		name: team_name,
+		created_by_id: created_by_id,
+		created_time: new Date().toISOString(),
+		roles: roles,
 	}
-	const requestBody = JSON.parse(event.body);
-		const { team_name, created_by_id, roles } = requestBody;
-		const TeamSchema = z.object({
-			name : z
-			.string()
-			.min(3, {
-				message: "Team name must be atleast 3 charachters long",
-			}),
-			created_by_id: z.string().uuid({
-				message: "Invalid created by id",
-			}),
-			created_time :  z.string().datetime(),
-			//check for roles names to be unique --- TO DO--
-			roles : z.array(
-				z.record(z.string(), z.string().uuid().array().nonempty())
-				)
-		})
-
-		const team = {
-			name: team_name,
-			created_by_id: created_by_id,
-			created_time: new Date().toISOString(),
-			roles: roles,
-		};
-		const result = TeamSchema.safeParse(team);
-		if (!result.success) {
-			return {
-				statusCode: 400,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Credentials": true,
-				},
-				body: JSON.stringify({
-					error: result.error.formErrors.fieldErrors,
-				}),
-			};
-		}
-		console.log(JSON.stringify(team))
-	const client = await connectToDatabase();
+	console.log(JSON.stringify(team))
+	const client = await connectToDatabase()
 	const query = `
                 update projects_table
                 set project = jsonb_set(
@@ -64,30 +39,21 @@ exports.handler = async (event) => {
                     true
                 )
                 where 
-                    id = $2`;
-	try {
-		const res = await client.query(query, [team, project_id]);
-		return {
-			statusCode: 200,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Credentials": true,
-			},
-			body: JSON.stringify({
-				message: "Team added to the project",
-			}),
-		};
-	} catch (error) {
-		console.error("Error updating data:", error);
-		return {
-			statusCode: 500,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Credentials": true,
-			},
-			body: JSON.stringify({ message: "Internal Server Error" }),
-		};
-	} finally {
-		await client.end();
+                    id = $2`
+	const res = await client.query(query, [team, project_id])
+	await client.end()
+	return {
+		statusCode: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Credentials": true,
+		},
+		body: JSON.stringify({
+			message: "Team added to the project",
+		}),
 	}
-};
+})
+	.use(authorize())
+	.use(bodyValidator(reqSchema))
+	.use(pathParamsValidator(idSchema))
+	.use(errorHandler())
