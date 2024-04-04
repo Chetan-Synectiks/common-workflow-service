d486e0b188d6f529b8611fc23b17f576eb6eb60d
SohailAli20 <mohammad.ali@synectiks.com>
2024-04-04 12:55:40 +0530

    improved error handling

--- workflow/util/errorHandler.js
+++ workflow/util/errorHandler.js
@@ -2,12 +2,11 @@ const {
 	UserNotConfirmedException,
 	NotAuthorizedException,
 	UsernameExistsException,
-} = require("@aws-sdk/client-cognito-identity-provider")
-const { AuthorizationError } = require("./authorizer")
+} = require("@aws-sdk/client-cognito-identity-provider");
+const { AuthorizationError } = require("./authorizer");
 
 exports.errorHandler = () => ({
 	onError: (handler) => {
-		console.log(handler.error.code);
 		if (handler.error instanceof AuthorizationError) {
 			handler.response = {
 				statusCode: 403,
@@ -17,9 +16,8 @@ exports.errorHandler = () => ({
 				body: JSON.stringify({
 					message: "unauthorised request",
 				}),
-			}
-		}
-		if (handler.error instanceof UserNotConfirmedException) {
+			};
+		} else if (handler.error instanceof UserNotConfirmedException) {
 			handler.response = {
 				statusCode: 403,
 				headers: {
@@ -28,9 +26,8 @@ exports.errorHandler = () => ({
 				body: JSON.stringify({
 					message: "email not verified",
 				}),
-			}
-		}
-		if (handler.error instanceof NotAuthorizedException) {
+			};
+		} else if (handler.error instanceof NotAuthorizedException) {
 			handler.response = {
 				statusCode: 409,
 				headers: {
@@ -39,9 +36,8 @@ exports.errorHandler = () => ({
 				body: JSON.stringify({
 					message: "Incorrect username or password.",
 				}),
-			}
-		}
-		if (handler.error instanceof UsernameExistsException) {
+			};
+		} else if (handler.error instanceof UsernameExistsException) {
 			handler.response = {
 				statusCode: 401,
 				headers: {
@@ -50,9 +46,8 @@ exports.errorHandler = () => ({
 				body: JSON.stringify({
 					message: "user already exists.",
 				}),
-			}
-		}
-		if (handler.error.code === 23505) {
+			};
+		} else if (handler.error.code === 23505) {
 			handler.response = {
 				statusCode: 401,
 				headers: {
@@ -61,17 +56,18 @@ exports.errorHandler = () => ({
 				body: JSON.stringify({
 					message: "Alrady exists.",
 				}),
-			}
-		}
-		handler.response = {
-			statusCode: 500,
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-			},
-			body: JSON.stringify({
-				message: handler.error.message,
-				error: handler.error,
-			}),
+			};
+		} else {
+			handler.response = {
+				statusCode: 500,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+				},
+				body: JSON.stringify({
+					message: handler.error.message,
+					error: handler.error,
+				}),
+			};
 		}
 	},
-})
+});
