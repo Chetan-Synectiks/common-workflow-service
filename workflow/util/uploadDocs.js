c2f7e76db4c87a73290f02c6166729beedf3cf54
Shiva Kumar Karinigi <shiva.karinigi@synectiks.com>
2024-04-10 10:54:11 +0530

    minor change uploadDocs function

--- workflow/util/uploadDocs.js
+++ workflow/util/uploadDocs.js
@@ -6,16 +6,16 @@ const s3Client = new S3Client({ region: "us-east-1" })
 module.exports.uploadToS3 = async (fileName, data) => {
 	const contentType = data.split(";")[0].split(":")[1]
 	const fileExtension = contentType.split("/")[1]
-	fileName = fileName.substring(0, fileName.lastIndexOf("."))
-	const newfileName = `${fileName
-		.replace(/ /g, "")
-		.toLowerCase()}_${Date.now()}.${fileExtension}`
+	// fileName = fileName.substring(0, fileName.lastIndexOf("."))
+	// const newfileName = `${fileName
+	// 	.replace(/ /g, "")
+	// 	.toLowerCase()}_${Date.now()}.${fileExtension}`
 	const bucket = process.env.BUCKET_NAME
 	const folder = process.env.BUCKET_FOLDER_NAME
 	const buffer = Buffer.from(data.split(",")[1], "base64")
 	const s3Params = {
 		Bucket: bucket,
-		Key: `${folder}${newfileName}`,
+		Key: `${folder}${fileName}`,
 		Body: buffer,
 		ContentType: contentType,
 	}
@@ -26,7 +26,7 @@ module.exports.uploadToS3 = async (fileName, data) => {
 		const statusCode = result.$metadata.httpStatusCode
 		if (statusCode === 200) {
 			console.log("sss")
-			const link = `https://${bucket}.s3.amazonaws.com/${folder}${newfileName}`
+			const link = `https://${bucket}.s3.amazonaws.com/${folder}${fileName}`
 			return { link, fileExtension, statusCode }
 		}
 	} catch (error) {
