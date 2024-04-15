543aa4234c6176db19f5e07cc1d087358e427a5b
Chetan Meegada <chetan.meegada@synectiks.com>
2024-04-15 11:02:40 +0530

    minor : updated api

--- workflow/workflow/addTemplate.js
+++ workflow/workflow/addTemplate.js
@@ -3,6 +3,7 @@ const { SFNClient, CreateStateMachineCommand } = require("@aws-sdk/client-sfn");
 const { generateStateMachine1 } = require("./generateStateMachine");
 const { z } = require("zod");
 const { v4: uuid } = require("uuid");
+const sfnClient = new SFNClient({ region: "us-east-1" });
 
 exports.handler = async (event) => {
   const { project_id, template_id } = JSON.parse(event.body);
@@ -20,43 +21,30 @@ exports.handler = async (event) => {
       }),
     };
   }
-
-  const templateSchema = z.string({ message: "Invalid template id" });
-  const tempValResult = templateSchema.safeParse(template_id);
-  if (!tempValResult.success) {
-    return {
-      statusCode: 400,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        error: tempValResult.error.issues[0].message,
-      }),
-    };
-  }
+  const workflowQuery =
+  "select workflow, name from master_workflow where id = $1";
+  const projectdata = "select project ->> 'name' as project_name from projects_table where id = $1";
+  const query = `
+  insert into workflows_table
+  (name, arn, metadata, project_id) values ($1, $2, $3::jsonb, $4::uuid)
+  returning *`;
   const client = await connectToDatabase();
   try {
     const metaData = {
       status: "inprogress",
     };
-    const workflowQuery =
-      "select workflow, name from master_workflow where id = $1";
     const workflpwResult = await client.query(workflowQuery, [template_id]);
     const stages = workflpwResult.rows[0].workflow.stages;
     const name = workflpwResult.rows[0].name;
     metaData.stages = stages;
-
-    const projectQueryPromise = client.query(
-      `select * from projects_table where id = $1`,
-      [project_id]
-    );
+    const projectQueryPromise = await client.query(projectdata, [project_id]);
+    const projectname = projectQueryPromise.rows[0].project_name;
     const workflowQueryPromise = client.query(
-      `SELECT COUNT(*) FROM workflows_table WHERE LOWER(SUBSTRING(name, POSITION('-' IN name) + 1)) = LOWER($1);;`,
+      `SELECT COUNT(*) FROM workflows_table WHERE LOWER(SUBSTRING(name, POSITION('-' IN name) + 1)) = LOWER($1);`,
       [name]
     );
 
-    const [projectResult, workflowExists] = await Promise.all([
-      projectQueryPromise,
+    const [ workflowExists] = await Promise.all([
       workflowQueryPromise,
     ]);
     if (workflowExists.rows[0].count > 0) {
@@ -70,11 +58,10 @@ exports.handler = async (event) => {
         }),
       };
     }
-    const sfnClient = new SFNClient({ region: "us-east-1" });
     const newStateMachine = generateStateMachine1(stages);
 
     const random = uuid().split("-")[4];
-    const workflowName = random + "@" + name.replace(/ /g, "_");
+    const workflowName = random + "@" + projectname.replace(/ /g, "_");
     const input = {
       name: workflowName,
       definition: JSON.stringify(newStateMachine),
@@ -84,11 +71,6 @@ exports.handler = async (event) => {
     const commandResponse = await sfnClient.send(command);
 
     metaData.created_time = new Date().toISOString();
-    let query = `
-                    insert into workflows_table
-                    (name, arn, metadata, project_id) values ($1, $2, $3::jsonb, $4::uuid)
-                    returning *`;
-
     const result = await client.query(query, [
       workflowName,
       commandResponse.stateMachineArn,
